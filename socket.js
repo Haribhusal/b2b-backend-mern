635e12960994ee7decc065e1f2fd57bed65e92a1
Haribhusal <bhusalhari89@gmail.com>
2024-08-29 13:13:31 +0545

    cors update 1

--- socket.js
+++ socket.js
@@ -7,10 +7,9 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app); // Create an HTTP server
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Update this to your client URL
+    origin: ["https://b2b-mern-frontend.vercel.app", "http://localhost:5173"],
     methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
-    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
   },
 });
 
